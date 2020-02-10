afee2888a1a6b1d7318eaae04e4695b71aa19415
Juan Manuel Pauluk <jmpauluk@github.com>
2020-02-10 16:50:31 -0300

    Resuelta toda la guia E/S

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,19 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+	var importe ;
+
+	var descuento;
+
+	importe = document.getElementById("importe").value ;
+
+	importe = parseInt(importe);
+
+	descuento = importe * 0.25 ; 
+
+	importe = importe - descuento;
+
+	document.getElementById("resultado").value = importe ; 
+
+
 }
