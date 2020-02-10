11e2da9597475b47394a75590561424803b88f33
alumno <alumno@utnfra.com.ar>
2020-02-10 20:10:33 -0300

    Guia 2 123

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,25 @@
 function mostrar()
 {
 //tomo la edad  
+  
+  var edad ; 
+
+  edad = document.getElementById("edad").value; 
+
+  if ( edad >= 18) {
+
+  	alert ("es mayor de edad");
+
+
+  	}
+  
+
+  else { 
+
+  	   alert("Es menor de edad");
+    }
+  
+
 
 
 }//FIN DE LA FUNCIÓN
