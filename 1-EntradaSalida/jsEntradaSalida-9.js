f26d55b0c8d0ecddc826b3aa0231357d43c1428e
Juan Manuel Pauluk <jmpauluk@github.com>
2020-02-10 16:44:52 -0300

    Resuelto toda la E/S

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -5,5 +5,21 @@ en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
 
+	var sueldo ;
+
+	var porcentaje ; 
+
+
+	sueldo = document.getElementById("sueldo").value; 
+
+	sueldo = parseInt (sueldo);
+
+
+	porcentaje = sueldo * 0.10 ;
+
+	sueldo = sueldo + porcentaje ; 
+
+	document.getElementById("resultado").value= sueldo ;
+	
 	
 }
