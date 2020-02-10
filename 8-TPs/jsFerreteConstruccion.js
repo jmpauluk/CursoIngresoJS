bb36c0948a45de5fa6aa73b40ef8473203da29c6
alumno <alumno@utnfra.com.ar>
2020-02-10 19:36:20 -0300

    Ejercicio 1 TP

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -3,11 +3,19 @@ A.	 mostrar la cantidad de alambre a comprar  si se ingresara el largo y el anch
 B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular y se debe alambra con tres hilos de alambre.
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
 */
+	var largoTe
+
+	var AnchoTe
+
+	var radioTe	
+
+	var resultado
+		
 
 function Rectangulo () 
-{
+	{
 
-}
+	}
 function Circulo () 
 {
 	
