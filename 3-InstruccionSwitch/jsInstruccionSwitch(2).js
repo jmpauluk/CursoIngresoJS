ddf9de80e7b8b9ecf56585a55b5d752555ae99b7
alumno <alumno@utnfra.com.ar>
2020-02-11 21:52:59 -0300

    Guia 3 2-3

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -3,9 +3,25 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+	switch(mesDelAño)
+	{
+		case "Julio":
+		case "Agosto":
+			alert("Abrigate");
+			break;
+		case "Enero":
+		case "Febrero":
+		case "Marzo":
+		case "Abril":
+		case "Mayo":
+		case "Junio":
+			alert("Falta para el invierno");
+			break;
 
+		default:
+			alert("Ya pasamos el frio, ahora calor!!!")	
 
+	}
 
 
 }//FIN DE LA FUNCIÓN
