0dbbcfefd32fba820519ef3b78ce0da1228703b3
Juan Manuel Pauluk <jmpauluk@github.com>
2020-02-15 10:58:48 -0300

    Guia 3 Resuelta

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -5,7 +5,30 @@ var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
 	
-	
+	switch(mesDelAño)
+
+	{
+		case "Abril":
+		case "Junio":
+		case "Septiembre":
+		case "Noviembre":
+			alert("Este mes tiene 30 dias");
+		break;
+		case "Enero":
+		case "Marzo":
+		case "Mayo":
+		case "Julio":
+		case "Agosto":
+		case "Octubre":
+		case "Diciembre":
+			alert("Este mes tiene 31 dias");
+		break;
+		
+		default:
+			alert("Este mes tiene 28 dias");
+
+
+	}
 
 
 
